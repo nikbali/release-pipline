7c7835fd0b04ae0675338bf49946ceef511db640
Nikita Balilyy <nikita.balilyy@open.ru>
2021-05-28 18:08:23 +0300

    init commit

---
+++ src/test/kotlin/com/nibali/demokotlin/DemoKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nibali.demokotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
