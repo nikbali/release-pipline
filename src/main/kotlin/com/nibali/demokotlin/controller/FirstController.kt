bf5476df1f2b5387c90e0fa634361c231260b177
Nikita Balilyy <nikita.balilyy@open.ru>
2021-05-28 21:47:39 +0300

    added first controller

---
+++ src/main/kotlin/com/nibali/demokotlin/controller/FirstController.kt
@@ -0,0 +1,16 @@
+package com.nibali.demokotlin.controller
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/first")
+class FirstController {
+
+    @GetMapping
+    fun get(): ResponseEntity<String> {
+        return ResponseEntity.ok("Its Ok")
+    }
+}
